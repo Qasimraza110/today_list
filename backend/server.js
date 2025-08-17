fce29c8d03408fc72d7610de3aa865ce35899d86
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-08-17 23:13:08 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ dotenv.config();
 const url = "mongodb://localhost:27017"
 const client = new MongoClient(url);
 
-// Database Name
 const dbName = "dailytask";
 const app = express();
 const port = 3000;
@@ -20,7 +19,6 @@ app.use(cors())
 
 client.connect();
 
-// GET all tasks
 app.get("/", async (req, res) => {
   const db = client.db(dbName);
   const collection = db.collection("tasks");
@@ -28,7 +26,7 @@ app.get("/", async (req, res) => {
   res.json(findResult);
 });
 
-// SAVE (Add) a task
+
 app.post("/", async (req, res) => {
   const data = req.body;
   const db = client.db(dbName);
@@ -37,24 +35,24 @@ app.post("/", async (req, res) => {
   res.send({ success: true, result });
 });
 
-// DELETE a task by id
+
 app.delete("/", async (req, res) => {
-  const { id } = req.body; // expect { id: "uuid" }
+  const { id } = req.body; 
   const db = client.db(dbName);
   const collection = db.collection("tasks");
   const result = await collection.deleteOne({ id });
   res.send({ success: true, result });
 });
 
-// EDIT (Update) a task
+
 app.put("/", async (req, res) => {
-  const { id, todo, isCompleted } = req.body; // expect full task object
+  const { id, todo, isCompleted } = req.body; 
   const db = client.db(dbName);
   const collection = db.collection("tasks");
 
   const result = await collection.updateOne(
-    { id }, // find task by id
-    { $set: { todo, isCompleted } } // update fields
+    { id }, 
+    { $set: { todo, isCompleted } } 
   );
 
   res.send({ success: true, result });
@@ -63,3 +61,4 @@ app.put("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
+
